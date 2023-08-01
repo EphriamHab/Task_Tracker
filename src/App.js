ddfa02eb076d5c8e27f54d33283dd17c575487c9
EphriamHab <ephremhabtamu2015@gmail.com>
2023-08-01 19:14:22 +0300

    Moadified on task

--- src/App.js
+++ src/App.js
@@ -24,12 +24,15 @@ function App() {
     }
 ])
 const deleteTask =(id)=>{
-  console.log('delete',id)
+  setTasks(tasks.filter((task)=>task.id !== id))
+}
+const toggleRemainder = (id)=>{
+
 }
   return (
     <div className="container">
      <Header title ='Task tracker'/>
-     <Tasks tasks={tasks} onDelete={deleteTask}/>
+     {tasks.length>0?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder}/>:'No Task to Show'}
     </div>
   );
 }
